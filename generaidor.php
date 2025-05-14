5011fce0726f919060ce1a8ed26836a1d37a9879
Cohekoma <dinhhoangbaochan@gmail.com>
2025-05-14 22:58:57 +0700

    remove unused statements

--- generaidor.php
+++ generaidor.php
@@ -4,17 +4,6 @@
 require_once __DIR__ . '/vendor/autoload.php';
 
 use Cohekoma\Generaidor\Parser\Yaml\YamlParser as YamlParser;
-use Cohekoma\Generaidor\Core\Paths as Paths;
-
-$allContentFiles = glob(Paths::CONTENT_DIR->getFullPath() . '/*.md');
-$samplePost = $allContentFiles[1];
-$samplePostContent = file_get_contents($samplePost);
-
-$metaDataRegEx = '/^---([\s\S]*?)---/';
-preg_match($metaDataRegEx, $samplePostContent, $matches);
-
-$metaData = explode("\n", trim($matches[1]));
-var_dump($metaData);
 
 $sample = new YamlParser();
-$sample->parse();
+var_dump($sample->parse());
