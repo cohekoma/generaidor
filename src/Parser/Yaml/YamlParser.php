0fdeb6cebad5dd619e5c7b28e21abdcfe2796857
Cohekoma <dinhhoangbaochan@gmail.com>
2025-05-14 22:58:36 +0700

    update parse method logic to try parsing a sample content

--- src/Parser/Yaml/YamlParser.php
+++ src/Parser/Yaml/YamlParser.php
@@ -3,10 +3,23 @@
 namespace Cohekoma\Generaidor\Parser\Yaml;
 
 use Cohekoma\Generaidor\Parser\ParserAbstraction;
+use Cohekoma\Generaidor\Core\Paths as Paths;
 
 class YamlParser extends ParserAbstraction {
-    public function parse() : void
+
+    /**
+     * @var string $metaDataRegEx Regex pattern to extract meta data from markdown file
+     */
+    private string $metaDataRegEx = '/^---([\s\S]*?)---/';
+
+    public function parse() : array|string
     {
-        echo "I'm parsing myself";
+        $allContentFiles = glob(Paths::CONTENT_DIR->getFullPath() . '/*.md');
+        $samplePost = $allContentFiles[1];
+        $samplePostContent = file_get_contents($samplePost);
+
+        preg_match($this->metaDataRegEx, $samplePostContent, $matches);
+
+        return explode("\n", trim($matches[1]));
     }
 }
