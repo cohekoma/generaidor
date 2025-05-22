f78f68c0a469f4326899478e9017db16594edb72
Cohekoma <dinhhoangbaochan@gmail.com>
2025-05-22 22:13:01 +0700

    #4 debug function from php forum to test

--- src/Services/SitemapService.php
+++ src/Services/SitemapService.php
@@ -28,16 +28,41 @@ class SitemapService {
             /**
              * @TODO: At this part right here, for each file path, we already have a flat array with the file name is the last item in the array. What we need to do here is to think about how to construct a tree-like array from a simple flat array, so that we can add into the final result. We can do like this: 1) Pop the last item. 2) Reverse the array. 3) Now, we will going backwards (from the bottom up), so we can wrap each element around the last item that we just popped.
              */
-            if (sizeof($filePathName) == 1) {
-                $result[] = $filePathName[0];
+/*            if ( sizeof($filePathName) > 1 ) {
+                $markdownFile = array_pop($filePathName);
+                $reversedFilePath = array_reverse($filePathName);
+                var_dump($reversedFilePath);
             } else {
-                var_dump(sizeof($filePathName));
-            }
+                $result[] = $filePathName;
+            }*/
 
-            var_dump($filePathName);
+//            var_dump($filePathName);
         }
 
-        var_dump($result);
+        $test = $this->dirToArray($contentDirPath);
+        var_dump($test);
         return $result;
+    }
+
+    public function dirToArray($dir): array
+    {
+        $result = array();
+        $cdir = scandir($dir);
+
+       foreach ($cdir as $key => $value) {
+          if (!in_array($value,array(".",".."))) {
+
+             if (is_dir($dir . DIRECTORY_SEPARATOR . $value)) {
+                $result[$value] = $this->dirToArray($dir . DIRECTORY_SEPARATOR . $value);
+             }
+
+            else {
+                $result[] = $value;
+            }
+          }
+       }
+
+        return $result;
+
     }
 }
