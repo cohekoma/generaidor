2710af99c86145e6105dd192541f76b50118b3f1
cohekoma <dinhhoangbaochan@gmail.com>
2026-01-03 14:42:26 +0700

    modify to use texttype enum

--- src/main.py
+++ src/main.py
@@ -1,6 +1,6 @@
-from textnode import TextNode
+from textnode import TextNode, TextType
 def main():
-    my_text = TextNode("This is some anchor text", "link", "https://bc.com")
+    my_text = TextNode("This is some anchor text", TextType.PLAIN, "https://bc.com")
     print(my_text)
 
 main()
--- src/textnode.py
+++ src/textnode.py
@@ -9,14 +9,13 @@ class TextType(Enum):
     ALT = "alt text"
 
 class TextNode():
-    def __init__(self, text, text_type, url):
+    def __init__(self, text, text_type: TextType, url):
         self.text = text
         self.text_type = text_type
         self.url = url
     
     def __eq__(self, value : TextNode):
-        print(f"comparing: {self.text} >< {value.text}")
-        return self.url == value.url and self.text_type == value.text_type and self.text == value.text
+        return self.url == value.url and self.text_type.name == value.text_type.name and self.text == value.text
 
     def __repr__(self):
-        return f"TextNode({self.text}, {self.text_type}, {self.url})"
+        return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
