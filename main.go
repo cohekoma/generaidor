53e2929dcd9606a1bc8ca4c96112f4b3b1ecab79
cohekoma <dinhhoangbaochan@gmail.com>
2026-02-08 14:50:44 +0700

    try reading through content dir

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/fs"
+	"path/filepath"
+)
 
+func main() {
+	contentRoot := "content"
+	// publicRoot := "public"
+
+	err := filepath.WalkDir(contentRoot, func(path string, dir fs.DirEntry, err error) error {
+		if err != nil {
+			return err
+		}
+
+		if dir.IsDir() {
+			return nil
+		}
+
+		fmt.Println(path)
+		return nil
+	})
+
+	if err != nil {
+		panic(err)
+	}
 }
