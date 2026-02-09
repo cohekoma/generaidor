2ebc4c46216bc2383b101aae0c6c55c09d74c893
cohekoma <dinhhoangbaochan@gmail.com>
2026-02-09 18:37:44 +0700

    read files from src then generate to dest

--- main.go
+++ main.go
@@ -3,23 +3,38 @@ package main
 import (
 	"fmt"
 	"io/fs"
+	"os"
 	"path/filepath"
 )
 
 func main() {
 	contentRoot := "content"
-	// publicRoot := "public"
+	publicRoot := "public"
 
 	err := filepath.WalkDir(contentRoot, func(path string, dir fs.DirEntry, err error) error {
 		if err != nil {
 			return err
 		}
 
-		if dir.IsDir() {
-			return nil
+		if path != contentRoot {
+			if !dir.IsDir() {
+				// err := os.Mkdir(publicRoot+dir.Name(), 0755)
+				srcFile := dir.Name()
+				srcFileExt := filepath.Ext(srcFile)
+				destFile := srcFile[:len(srcFile)-len(srcFileExt)] + ".html"
+
+				generatedFile, err := os.Create(publicRoot + "/" + destFile)
+
+				if err != nil {
+					panic(err)
+				}
+
+				fmt.Println("Generated file: " + generatedFile.Name())
+				// fmt.Println("Making file in: " + publicRoot + dir.Name())
+			}
 		}
 
-		fmt.Println(path)
+		// fmt.Println(path)
 		return nil
 	})
 
