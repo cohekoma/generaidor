280edb5cf68e5a416b93b8b6bec1ef242eb8c7d4
cohekoma <dinhhoangbaochan@gmail.com>
2026-05-09 15:12:16 +0700

    config default usage guide

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -13,7 +13,7 @@ func Init() {
 
 	if len(os.Args) < 2 {
 		fmt.Println("You need to supply at least one command for Generaidor to run. Please refer to usage!")
-		flag.Usage()
+		printDefaultUsage()
 		os.Exit(1)
 	}
 
@@ -27,13 +27,27 @@ func Init() {
 	case "build":
 		buildCmd.Parse(os.Args[2:])
 		runBuildCmd()
+
+	default:
+		fmt.Println("Unknown command. Please refer to usage guide below.")
+		printDefaultUsage()
+		os.Exit(1)
 	}
+}
+
+func printDefaultUsage() {
+	fmt.Println("Usage: generaidor <command> [options]")
+	fmt.Println("Available commands:")
+	fmt.Println("\tscaffold: Create default structure for your project.")
+	fmt.Println("\tconfig: Config custom directories to scan and ouput result.")
+	fmt.Println("\tbuild: Generate static content based on the content directory.")
 }
 
 func initBuildCmd() *flag.FlagSet {
 	cmd := flag.NewFlagSet("build", flag.ExitOnError)
-	outArg := cmd.String("out", "public", "Set a directory to output static content.")
-	fmt.Println("outing...", outArg)
+	outArgs := cmd.String("out", "public", "Set a directory to output static content.")
+	_ = outArgs
+
 	return cmd
 }
 
